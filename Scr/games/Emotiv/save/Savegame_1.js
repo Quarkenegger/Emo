113bee0a4a10a9461e7db630a345a22b390dd7e5
Johannes Zeiße <johannes.zeisse@uni-potsdam.de>
2017-05-25 17:53:07 +0200

    shortcuttutorial

--- Scr/games/Emotiv/save/Savegame_1.js
+++ Scr/games/Emotiv/save/Savegame_1.js
@@ -1,2 +1,2 @@
-save={map:'kulturzentrum_eingang.rmp',person_x:362,person_y:237,direction:'east',inv:[],emo:[1],cquest:[],squest:[],trigger_pr:[1],map_pr:[1,1,1,0,0,0],volume:0}
+save={map:'Detektei_Polizei_Buero.rmp',person_x:657,person_y:1035,direction:'south',inv:[],emo:[1],cquest:[1],squest:[],trigger_pr:[5],map_pr:[1,1,1,0,0,0],volume:0}
 
