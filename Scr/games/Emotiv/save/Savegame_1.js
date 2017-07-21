a6edfa78776b6fb517373998483e7a09214f4dab
Johannes Zeiße <johannes.zeisse@uni-potsdam.de>
2017-07-21 09:16:59 +0200

    triggerfixes

--- Scr/games/Emotiv/save/Savegame_1.js
+++ Scr/games/Emotiv/save/Savegame_1.js
@@ -1,2 +1,2 @@
-save={map:'Park.rmp',person_x:824,person_y:1495,direction:'north',inv:[],emo:[1],cquest:[],squest:[],trigger_pr:[3001],map_pr:[1,1,1,0,0,0],volume:0.2,emomap:true}
+save={map:'PloizeiHaupt.rmp',person_x:133,person_y:132,direction:'south',inv:[],emo:[1],cquest:[1],squest:[],trigger_pr:[5],map_pr:[1,1,1,0,0,0],volume:0,emomap:true}
 
