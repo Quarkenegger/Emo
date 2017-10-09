248263ee860187ce8b9e06ba0b78afa93feb3809
klopskerl <aq5saq5s12>
2017-10-10 00:45:44 +0200

    bilder gefixt, endlosschleife als strafe hinzugefügt

---
+++ Scr/games/Emotiv/save/Savegame_1 - Kopie.js
@@ -0,0 +1,5 @@
+save={map:'Detektei_Polizei_Buero.rmp',person_x:1003,person_y:654,direction:'west',inv:[],emo:[1,18,3,28,19,16,14],cquest:[6],squest:[2,1,4,5,3],trigger_pr:[1001,6],map_pr:[1,1,1,0,0,0],volume:0.2,emomap:true}
+
+,
+map_17:{name:'Labor',description:'nothing',realname:'Labor.rmp',music:'MAPlabor.mp3'}
+{name:'Galerie',description:'Hier standen schon so viele Meisterwerke. Die Kuenstler werden von Jahr zu Jahr besser.', realname:'kulturzetrum_eingang.rmp',music:'MAPgallery.mp3'}
