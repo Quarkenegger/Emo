807fd6df198a5cd0813ccc7d2d39ba00839a044c
klopskerl <aq5saq5s12>
2017-10-10 00:45:17 +0200

    bilder gefixt, endlosschleife als strafe hinzugefügt

--- Scr/games/Emotiv/save/Savegame_1.js
+++ Scr/games/Emotiv/save/Savegame_1.js
@@ -1,2 +1,2 @@
-save={map:'Wohnviertel.rmp',person_x:615,person_y:135,direction:'west',inv:[],emo:[1,18,3,16,28,19,14,12,40],cquest:[6,9],squest:[2,1,4,5,3,8,7],trigger_pr:[1005,4],map_pr:[1,1,1,0,1,0],volume:0.2,emomap:true}
+save={map:'Park.rmp',person_x:287,person_y:1398,direction:'west',inv:[301],emo:[1,18,3,28,19,16,14,12,40,23,31,20,34,8],cquest:[14],squest:[2,1,4,5,3,8,7,9,10,6,11,18,19,17,12,13],trigger_pr:[3002,4],map_pr:[1,1,1,1,1,1],volume:0.2,emomap:true}
 
